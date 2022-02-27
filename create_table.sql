28c32239145cda7e3e3de3538bf6ca5de917799d
kodemafia <dharmendramehta622@gmail.com>
2022-02-27 19:29:35 +0545

    create, read and insert into table done

--- create_table.sql
+++ create_table.sql
@@ -1,1 +1,7 @@
-create table userinfo;
+CREATE TABLE personal (
+id INT,
+name VARCHAR(50),
+birth_date DATE,
+phone VARCHAR(12),
+gender varchar(1)
+);
---
+++ insert_data_into_table.sql
@@ -0,0 +1,1 @@
+insert into personal (id, name, birth_date, phone,gender) values (1,'Ram Kumar',"1999-10-29", "9819063457","male");
